6b3d9f70aa854a242ebfaa3bb72deb9e1aa54a72
Kizito-Alberrt <wagabi98@gmail.com>
2021-02-24 00:26:54 +0300

    removed category in models.py

--- app/models.py
+++ app/models.py
@@ -3,13 +3,12 @@ class News:
     news class to define news objects
     '''
 
-    def __init__(self,id,name,title,description,urlToImage,publishedAt,category):
+    def __init__(self,id,name,title,description,urlToImage,publishedAt):
         self.id =id
         self.title = title
         self.name = name
         self.description = description
         self.urlToImage = urlToImage
         self.publishedAt = publishedAt
-        self.category = category
 
 
